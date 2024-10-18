c65275a77e913cfe41a5a2ced01363d1174c1d2a
Akahara <albincalais@gmail.com>
2024-10-18 11:21:21 -0400

    feat: working installer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
- # RUN THE BLOODY INSTALLER !!!!
+### Installation
+
+Clone with `git clone --recurse-submodules https://github.com/Akahara/Pyrite`.
+
+Run `install_vcpkg_assimp.bat`, run visual studio, set PyriteEditor as your startup project and set your debug directory (project->settings->Debugging) to `$(ProjectDir)\runtime`.
--- install_vcpkg_assimp.bat
+++ install_vcpkg_assimp.bat
@@ -1,32 +1,14 @@
 @echo off
 SET VCPKG_PATH=C:\vcpkg
 
-REM Check if vcpkg is already installed
 if exist "%VCPKG_PATH%" (
     echo vcpkg is already installed at %VCPKG_PATH%.
-    exit /b
-)
-
-REM Prompt the user for installation
-set /p response="Do you want to install vcpkg to %VCPKG_PATH%? (Y/N): "
-
-if /I "%response%"=="Y" (
-    REM Clone vcpkg repository
-    git clone https://github.com/Microsoft/vcpkg.git "%VCPKG_PATH%"
-
-    REM Change directory to vcpkg
-    cd "%VCPKG_PATH%"
-
-    REM Bootstrap vcpkg
-    call bootstrap-vcpkg.bat
-
-    REM Install assimp using vcpkg
-    call vcpkg.exe install assimp
-
-    REM Add vcpkg to the system PATH
-    setx PATH "%PATH%;%VCPKG_PATH%"
-
-    echo vcpkg and assimp have been installed and added to your PATH. Please restart your terminal.
 ) else (
-    echo Installation cancelled.
+    git clone https://github.com/Microsoft/vcpkg.git "%VCPKG_PATH%"
+    cd "%VCPKG_PATH%"
+    call bootstrap-vcpkg.bat
+	call vcpkg.exe integrate install
+    setx PATH "%PATH%;%VCPKG_PATH%"
 )
+
+call vcpkg.exe install assimp
